b1e61a9d2c2a724c3ff390c9fd0bb5e3b58f91d7
seanchenye <seanchenye1997@gmail.com>
2019-01-17 07:21:07 -0500

    20190117

--- app/src/main/java/com/bd/shuyu/motiontrackingandroid/MainActivity.java
+++ app/src/main/java/com/bd/shuyu/motiontrackingandroid/MainActivity.java
@@ -3,16 +3,44 @@ package com.bd.shuyu.motiontrackingandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.widget.TextView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final String TAG = "MainActivity";
+    JavaCameraView javaCameraView;
+    Mat mRgba;
+    /*
+    Dedicated for the onResume method
+     */
+    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+
+            switch (status){
+                case BaseLoaderCallback.SUCCESS:{
+                    javaCameraView.enableView();
+                    break;
+                }
+                default:{
+                    super.onManagerConnected(status);
+                }
+            }
+        }
+    };
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3");
         if(OpenCVLoader.initDebug()){
             Log.d(TAG, "OpenCV successfully loaded");
         }
@@ -27,13 +55,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //TextView tv = (TextView) findViewById(R.id.sample_text);
+        //javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        //javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        //javaCameraView.setCvCameraViewListener(this);
+        //tv.setText(stringFromJNI());
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(javaCameraView!=null) javaCameraView.disableView();
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(javaCameraView!=null) javaCameraView.disableView();
+    }
+    /*
+    Everytime the App resume, we need to call BaseLoaderCallback
+     */
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV successfully loaded");
+            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+        else{
+            Log.d(TAG, "not loaded");
+        }   OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallBack);
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    */
+    //public native String stringFromJNI();
+
+
+    //3 method for the listener
+    @Override
+    public void onCameraViewStarted(int w, int h){
+        //we have 4 channels here
+        mRgba = new Mat(h, w, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped(){
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame){
+        mRgba = inputFrame.rgba();
+        return mRgba;
+    }
 }
