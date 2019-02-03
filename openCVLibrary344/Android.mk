78cb8bb7b09b9b3d5d5c7d9b099be6930c11ebd4
seanchenye <seanchenye1997@gmail.com>
2019-02-03 10:11:20 -0500

    20190203_2

--- openCVLibrary344/Android.mk
+++ openCVLibrary344/Android.mk
@@ -4,14 +4,14 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := openCVLibrary344
 LOCAL_LDFLAGS := -Wl,--build-id
 LOCAL_SRC_FILES := \
-	src\sdk\native\jni\include\opencv\*.hpp \
-	src\sdk\native\jni\include\opencv2\*\*.hpp \
-	src\sdk\native\jni\include\opencv2\*\*\*.hpp \
-	src\sdk\native\jni\include\opencv2\*\*\*\*.hpp \
-	src\sdk\native\jni\include\opencv2\*\*\*\*\*.hpp \
+	sdk\native\jni\include\opencv\*.hpp \
+	sdk\native\jni\include\opencv2\*\*.hpp \
+	sdk\native\jni\include\opencv2\*\*\*.hpp \
+	sdk\native\jni\include\opencv2\*\*\*\*.hpp \
+	sdk\native\jni\include\opencv2\*\*\*\*\*.hpp \
 
 #LOCAL_C_INCLUDES += F:\Android_Projects\MotionTrackingAndroid\openCVLibrary344\src\debug\jni
 #LOCAL_C_INCLUDES += F:\Android_Projects\MotionTrackingAndroid\openCVLibrary344\src\main\jni
-LOCAL_C_INCLUDES += F:\Android_Projects\MotionTrackingAndroid\openCVLibrary344\src\sdk\native\jni\include
+#LOCAL_C_INCLUDES += F:\Android_Projects\MotionTrackingAndroid\openCVLibrary344\sdk\jni\include
 
 include $(BUILD_SHARED_LIBRARY)
