f3f716a61245cb74f598e6d475af5fbca3c5df6d
seanchenye <seanchenye1997@gmail.com>
2019-02-04 23:14:31 -0500

    20190204_Audio_PCM8

--- app/src/main/java/com/bd/shuyu/motiontrackingandroid/SignalGen/ArduinoFeeder.java
+++ app/src/main/java/com/bd/shuyu/motiontrackingandroid/SignalGen/ArduinoFeeder.java
@@ -15,20 +15,22 @@ public class ArduinoFeeder {
 
     // originally from http://marblemice.blogspot.com/2010/04/generate-and-play-tone-in-android.html
     // and modified by Steve Pomeroy <steve@staticfree.info>
-    private static final int fps = 30;
+    public static final int fps = 30;
     private static final float duration = (float)1/fps; // seconds
     private static final int sampleRate = 8000;
     private static final int numSamples = (int) (duration * sampleRate);
     private static final double sample[] = new double[numSamples];
     //private static final double freqOfTone = 440; // hz
 
-    private static final byte generatedSnd[] = new byte[2 * numSamples];
-
+    //private static final byte generatedSnd[] = new byte[2 * numSamples];
+    private static final byte generatedSnd[] = new byte[numSamples];
     public static final String TAG = "AudioGen";
 
 
 
     public static void genTone(double freqOfTone, double loudness){
+        short val = 0;
+        /*
         // fill out the array
         for (int i = 0; i < numSamples; ++i) {
             sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
@@ -43,18 +45,35 @@ public class ArduinoFeeder {
             // in 16 bit wav PCM, first byte is the low order byte
             generatedSnd[idx++] = (byte) (val & 0x00ff);
             generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
-
-
         }
-        Log.d(TAG, "generatedLength: " + generatedSnd.length);
+        */
+        long sum = 0;
+        for(int i = 0; i < numSamples; i++){
+            //val = (short) (Math.sin(2 * Math.PI * freqOfTone * i / sampleRate)
+            //        * 127 * loudness);
+            //Log.d(TAG, "VALUE:  " + val);
+            //generatedSnd[i] = (byte) (val & 0x00ff);
+
+            byte sp = (byte)(Math.sin(2 * Math.PI * freqOfTone * i / sampleRate)
+                    * 127 * loudness + 127) ;
+            generatedSnd[i] = sp;
+            sum += sp;
+            //generatedSnd[2*i] = sp;
+            //generatedSnd[2*i+1] = (byte) 0;
+        }
+        Log.d(TAG, "GEN AVG:  " + (float) sum/generatedSnd.length);
+
+
+        //Log.d(TAG, "generatedLength: " + generatedSnd.length);
     }
 
     public static void playSound(){
         final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
                 sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+                AudioFormat.ENCODING_PCM_8BIT, generatedSnd.length,
                 AudioTrack.MODE_STATIC);
-
+        //Log.d(TAG, "  " + audioTrack.getBufferSizeInFrames());
+        //audioTrack.write(generatedSnd, 0, generatedSnd.length);
         audioTrack.write(generatedSnd, 0, generatedSnd.length);
         audioTrack.play();
         audioTrack.release();
