ccec78ee77f7c930bf3bcf279ecbe22422c3e525
seanchenye <seanchenye1997@gmail.com>
2019-02-19 22:11:38 -0500

    20190219

--- app/src/main/java/com/bd/shuyu/motiontrackingandroid/SignalGen/ArduinoFeeder.java
+++ app/src/main/java/com/bd/shuyu/motiontrackingandroid/SignalGen/ArduinoFeeder.java
@@ -17,13 +17,13 @@ public class ArduinoFeeder {
     // originally from http://marblemice.blogspot.com/2010/04/generate-and-play-tone-in-android.html
     // and modified by Steve Pomeroy <steve@staticfree.info>
     public static final int fps = 30;
-    private static final float duration = (float)1/fps; // seconds
+    private static double duration = 1/fps; // seconds
     private static int sampleRate = 48000;
     private static int numSamples = (int) (duration * sampleRate);
-    private static double sample[] = new double[numSamples];
+
     //private static final double freqOfTone = 440; // hz
 
-    private static byte generatedSnd[] = new byte[2 * numSamples];
+    private static byte generatedSnd[];
     //private static byte generatedSnd[] = new byte[numSamples];
 
 
@@ -31,59 +31,92 @@ public class ArduinoFeeder {
         if(sampleRate == sr) return;
 
         sampleRate = sr;
-        numSamples = (int) (duration * sampleRate);
-        sample = new double[numSamples];
-        generatedSnd = new byte[numSamples];
+
     }
 
-    public static void genTone(double freqOfTone, double loudness){
-        short val = 0;
 
-        // fill out the array
-        for (int i = 0; i < numSamples; ++i) {
-            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
-        }
-
-        // convert to 16 bit pcm sound array
-        // assumes the sample buffer is normalised.
-        int idx = 0;
-        for (final double dVal : sample) {
-            // scale to maximum amplitude
-            val = (short) ((loudness * dVal * 32767));
-            // in 16 bit wav PCM, first byte is the low order byte
-            generatedSnd[idx++] = (byte) (val & 0x00ff);
-            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
-        }
+    public synchronized static byte[] genTone(double dur, double freqOfTone, double loudness, RetDouble phase_sin) {
+        duration = dur;
+        numSamples = (int) Math.floor(duration * sampleRate);
         /*
-        //long sum = 0;
-        for(int i = 0; i < numSamples; i++){
-            //val = (short) (Math.sin(2 * Math.PI * freqOfTone * i / sampleRate)
-            //        * 127 * loudness);
-            //Log.d(TAG, "VALUE:  " + val);
-            //generatedSnd[i] = (byte) (val & 0x00ff);
-
-            byte sp = (byte)(Math.sin(2 * Math.PI * freqOfTone * i / sampleRate)
-                    * 127 * loudness + 127) ;
-            generatedSnd[i] = sp;
-            //sum += sp;
-            //generatedSnd[2*i] = sp;
-            //generatedSnd[2*i+1] = (byte) 0;
-        }
-        //Log.d(TAG, "GEN AVG:  " + (float) sum/generatedSnd.length);
+        To Make The Signal Length Integer Multiple of the Frequency(WaveLength)
+        double SAMPLES_PER_WAVELENGTH = 1 / freqOfTone * sampleRate;
+        //int tmpSamples = (int) Math.floor(Math.floor(numSamples / SAMPLES_PER_WAVELENGTH) * SAMPLES_PER_WAVELENGTH);
+        generatedSnd = new byte[tmpSamples];
         */
 
-        //Log.d(TAG, "generatedLength: " + generatedSnd.length);
+
+
+        generatedSnd = new byte[numSamples];
+        for (int i = 0; i < numSamples; i++) {
+
+            byte sp = (byte) (Math.sin(2 * Math.PI * freqOfTone * i / sampleRate + phase_sin.getVal())
+                    * 127 * loudness + 127);
+            generatedSnd[i] = sp;
+
+        }
+
+        phase_sin.setVal(Math.asin(Math.sin( 2*Math.PI * freqOfTone * numSamples / sampleRate + phase_sin.getVal())));
+        Log.d(TAG, "phase updated:  " + phase_sin.getVal()/(2*Math.PI) + "pi");
+        /*
+        numSamples = numSamples - 1;
+
+        //'PADDING' the REMAINDINGS!!!!!!!!
+        byte paddingSnd[] = new byte[numSamples-tmpSamples];
+        int paddingLength = numSamples - tmpSamples;
+        int freqOfPadding = 0;
+        if(paddingLength > (double) 0.5 /freqOfTone * sampleRate){
+
+            freqOfPadding = (int) Math.floor(1/paddingLength*sampleRate);
+
+            Log.d(TAG, "larger than HALF WAVELENGTH");
+
+        }else if(paddingLength < (double) 0.5 /freqOfTone * sampleRate){
+
+            freqOfPadding = (int) Math.floor(0.5 / paddingLength*sampleRate);
+
+            Log.d(TAG, "smaller than HALF WAVELENGTH");
+
+        }
+
+        //generate a sin function from '0' to ensure phase!
+        for (int i = 0; i<paddingLength; i++) {
+
+            byte sp = (byte) (Math.sin(2 * Math.PI * freqOfPadding * i / sampleRate)
+                    * 127 * loudness + 127);
+            generatedSnd[i + tmpSamples] = sp;
+        }
+        */
+        return generatedSnd;
     }
 
     public static void playSound(){
+
+
         final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
                 sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
-                AudioTrack.MODE_STATIC);
-        //Log.d(TAG, "  " + audioTrack.getBufferSizeInFrames());
+                AudioFormat.ENCODING_PCM_8BIT, generatedSnd.length,
+                AudioTrack.MODE_STATIC);  // WAS MODE_STATIC
+
         //audioTrack.write(generatedSnd, 0, generatedSnd.length);
         audioTrack.write(generatedSnd, 0, generatedSnd.length);
         audioTrack.play();
         audioTrack.release();
     }
+    /*
+    void setThreadAffinity(){
+
+        int currentCPU = sched_getcpu();
+        pid_t threadId = getid();
+        cpu_set_t cpu_set;
+        CPU_ZERO(cpu_set);
+        CPU_SET(currentCPU, cpu_set);
+        sche_setaffinity(threadId, sizeof(cpu_set_t), cpu_set);
+    }
+
+    public static void playSound_AAudio(){
+
+
+    }
+    */
 }
